447e6f0874cbadcb66699f2829c51e4de372bef6
Sandeep <sandipsharma2445@gmail.com>
2024-03-17 11:35:32 +0530

    Performance Improved

--- script.js
+++
@@ -1,9 +0,0 @@
-const navBtn = document.getElementById("nav-btn")
-
-navBtn.addEventListener("click", toggleNavbar)
-
-function toggleNavbar() {
-    navBtn.getAttribute("aria-expanded") === "true"
-        ? navBtn.setAttribute("aria-expanded", false)
-        : navBtn.setAttribute("aria-expanded", true)
-}
